a21acf14b4beaf66c5a9c079063e18c08c7fac1d
kitsuneMai <dnldlgd95@gmail.com>
2025-04-01 11:01:30 -0500

    Generamos la estructura principal

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-4">
+      <h1 className="text-3xl font-bold">¡Tailwind está funcionando con rojo!</h1>
+    </div>
+  );
+}
+
+export default App;
+
